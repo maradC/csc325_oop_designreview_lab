c46e1d1003f0efb98ab1816742e79cde1fb3fc7a
maradC <maradiagachristopher5@gmail.com>
2024-11-01 10:26:49 -0400

    Adding Comments to Student Class

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -11,29 +11,51 @@ package com.mycompany.csc325_oop_designreview_lab;
 public class Student extends Human {
     private double GPA;
 
-	// ToDo 1: Make this class a child of Human
+    /**
+     * Constructor for the Student class.
+     * Initializes the student's name, age, and GPA.
+     *
+     * @param name the name of the student
+     * @param age the age of the student
+     * @param GPA the GPA of the student
+     */
     public Student(String name, short age, double GPA){
         super(name, age);
         this.GPA = GPA;
     }
-
-	// ToDo 2: Fix the resulting errors
-
-	// ToDo 3: Add a field for GPA and create a setter and a getter
-
+    /**
+     * Gets the GPA of the student.
+     *
+     * @return the GPA as a double
+     */
     public double getGPA() {
         return GPA;
     }
-
+    /**
+     * Sets the GPA of the student.
+     *
+     * @param GPA the new GPA value to set
+     */
     public void setGPA(double GPA) {
         this.GPA = GPA;
     }
 
+    /**
+     * Gets the address of the student.
+     * Overrides the abstract method from the Human class.
+     *
+     * @return the address as a String
+     */
     @Override
     public String getAddress() {
         return super.address;
     }
-
+    /**
+     * Sets the address of the student.
+     * Overrides the abstract method from the Human class.
+     *
+     * @param address the new address to set
+     */
     @Override
     public void setAddress(String address) {
         super.address = address;
