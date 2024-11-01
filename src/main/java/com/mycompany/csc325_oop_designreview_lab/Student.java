1c7a0f7d940081cc6b58e9c02ae393af78be8d6d
maradC <maradiagachristopher5@gmail.com>
2024-11-01 10:09:55 -0400

    Created GPA Field

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -8,12 +8,15 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author MoaathAlrajab
  */
-public class Student {
+public class Student extends Human {
+    private double GPA;
+
 	// ToDo 1: Make this class a child of Human
+    public Stu
 
 	// ToDo 2: Fix the resulting errors
 
 	// ToDo 3: Add a field for GPA and create a setter and a getter
-	
+
 	// ToDo 4: Add comments to your code
 }
